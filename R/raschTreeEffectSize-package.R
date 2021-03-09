6c4aaa88ec75532e2a952004dab93e336897bcd4
Mirka Henninger <mail@mirka-henninger.de>
2021-03-09 19:20:17 +0100

    improve package dependencies

---
+++ R/raschTreeEffectSize-package.R
@@ -0,0 +1,15 @@
+#' raschTreeEffectSize: Helper functions to use the Mantel-Haenszel effect size measure as a stopping criterion in Raschtrees
+#'
+#' @docType package
+#' @name raschTreeEffectSize-package
+#' @aliases raschTreeEffectSize
+#' @author Mirka Henninger
+#'
+#' @description Helper functions to use the Mantel-Haenszel effect size measure as a stopping criterion in Raschtrees
+#'
+#' @import psychotree
+#' @import partykit
+#' @import difR
+#' @importFrom stats pchisq
+#'
+NULL
