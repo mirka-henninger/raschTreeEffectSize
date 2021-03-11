908ee3a75bb637d17363f1a8d3c2085631adc7f9
Mirka Henninger <mail@mirka-henninger.de>
2021-03-11 17:38:53 +0100

    add checks to the color-byNode function

--- R/color_byNode.R
+++ R/color_byNode.R
@@ -9,15 +9,16 @@
 #' @examples
 #' \dontrun{
 #' data("DIFSim", package = "psychotree")
-#' rt <- raschtree(resp ~ age + gender + motivation, data = DIFSim, verbose = TRUE)
-#' rt_MH <- add_mantelHaenszel(rt, purification = "iterative")
-#' plot(rt_MH, inner_panel = show_ETSMH, terminal_panel = color_byNode(nodeID = 1, color = c("blue", "orange", "darkgreen")))
+#' RT <- raschtree(resp ~ age + gender + motivation, data = DIFSim, verbose = TRUE)
+#' RT_MH <- add_mantelHaenszel(RT, purification = "iterative")
+#' plot(RT_MH, inner_panel = show_ETSMH, terminal_panel = color_byNode(nodeID = 1, color = c("blue", "orange", "darkgreen")))
 #' }
 #'
 #' @export
 color_byNode <- function(nodeID, color = c("green", "orange", "red")) {
   return_colorFun <- function(object, ...){
-    psychotree::node_profileplot(object, col = create_colorList(object, nodeID = nodeID, cols = color))
+    returnFun <- psychotree::node_profileplot(object, col = create_colorList(object, nodeID = nodeID, cols = color))
+    return(returnFun)
   }
   class(return_colorFun) <- "grapcon_generator"
   return(return_colorFun)
@@ -30,15 +31,25 @@ color_byNode <- function(nodeID, color = c("green", "orange", "red")) {
 #'
 #' @return A list named after the terminal nodes containing the colors of the item parameter based on ETS Mantel-Haenszel classification
 create_colorList <- function(object, nodeID, cols){
+  # check whether Delta-MH is saved in the Raschtree object
+  if(is.null(object$info$mantelHaenszel))
+    stop("No Mantel-Haenszel classification found. Please use the add_mantelHaenszel function to add Mantel-Haenszel effect size measures to the Raschtree object")
+
   # get info on end nodes
   nodes <- partykit::node_party(object) # where the splits are
   innerNodes <- get_innerNodes(nodes)
-  whichID <- which(sapply(innerNodes, function(x) x$id) == nodeID)
+  innerNodesIDs <- sapply(innerNodes, function(x) x$id)
+
+  # check whether nodeID describes an inner node
+  if(!(nodeID %in% innerNodesIDs)) stop(c("Coloring can only be executed based on ETS-MH of inner nodes (inner nodes: ", paste(as.character(innerNodesIDs), collapse = ", "), ")"))
+
+  # get terminal nodes of nodeID and the remaining node IDs
+  whichID <- which(innerNodesIDs == nodeID)
   terminalNodes <- get_terminalNodes(innerNodes[[whichID]])
   allTerminalNodes <- get_terminalNodes(nodes)
   otherTerminalNodes <- allTerminalNodes[!allTerminalNodes %in% terminalNodes]
 
-  # color
+  # create the color list
   MHclassi <- object$info$mantelHaenszel$classification
   colorInfo <- data.frame(MHclassi)[,which(colnames(MHclassi) == paste("node", nodeID, sep = ""))]
   colFun <- function(x){ifelse(x == "A", cols[1], ifelse(x == "B", cols[2], ifelse(x == "C", cols[3], x)))}
